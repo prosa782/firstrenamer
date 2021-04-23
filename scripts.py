2b201dfa6e3fe875e2d2afbdc7418180599fbe4f
Balaji Shinnosuke <62749324+bluv-pr@users.noreply.github.com>
2021-04-23 18:56:27 +0530

    Add files via upload

---
+++ scripts.py
@@ -0,0 +1,53 @@
+class Scripted(object):    
+
+
+    PROGRESS_DIS = """\n
+<b>📁 : {1} | {2}</b>\n
+<b>♂️ : {0}%</b>\n
+<b>⚡ : {3}/s</b>\n
+<b>⏱️ : {4}</b>\n"""
+
+
+    HELP_TEXT = """
+    Send a photo to make it as thumbnail (optional)\n
+Send me any file (or) Media from telegram\n
+Convert files into video use /convert command\n
+Reply to that file with /rename new name.ext\n
+View your thumbnail do /savethumb command\n
+Delete your thumbnail do /delthumb command"""
+
+
+    ABOUT_TEXT = """
+<b>My Name🤭 :</b> <a href='http://t.me/TN57_Renamer_Bot'>TN57 Renamer BoT</a>\n
+<b>Channel :</b> <a href='https://t.me/TN57_BotZ'>TN57_BotZ</a>\n
+<b>Version :</b> <a href='http://t.me/TN57_Renamer_Bot'>0.9.2 beta</a>\n
+<b>Source :</b> <a href='https://t.me/BLuVDS'>Click Here</a>\n
+<b>Server :</b> <a href='https://heroku.com'>Heroku</a>\n
+<b>Library :</b> <a href='https://github.com/pyrogram'>Pyrogram 1.2.8</a>\n
+<b>Language :</b> <a href='https://www.python.org'>Python 3.9.4</a>\n
+<b>Developer :</b> <a href='https://t.me/BLuVDS'>✯ °•Bᴀʟᴀᴊɪ•° ✯</a>\n
+<b>Support Group :</b> <a href='https://t.me/TN57_BotzSupport'>TN57_BotZ Support</a>\n"""
+
+
+    CUSTOM_CAPTION = "<i>{}</i>"
+    PROCESSING_TEXT = "Processing..."
+    ACCESS_DENIED = "¥ou Are Banned 🚫"
+    BANNED_USER_TEXT = "¥ou Are Banned 🚫"
+    TRYING_TO_UPLOAD = "Trying to upload...👻"
+    RENAMED_SUCCESS = "Renaming completed ✓"
+    UPLOAD_SUCCESS = "Thank u for Using Me @TN57_BotZ For More Updates"
+    THUMBNAIL_SAVED = "Your Thumbnail Saved ✅"
+    NO_THUMBNAIL_FOUND = "No Thumbnail Found 😟"
+    TRYING_TO_DOWNLOAD = "Trying To Download...😼"
+    CURRENT_THUMBNAIL = "Your Current Thumbnail 💫"
+    THUMBNAIL_DELETED = "Your Thumbnail Deleted ✅"
+    RENAMING_VIDEO = "Renaming your Video please wait.."
+    REPLY_TO_MEDIA = "Reply to that Media with /convert"
+    UPLOAD_START = "Uploading your file please wait...👻\n"
+    DOWNLOAD_START = "Downloding your file please wait...😼\n"
+    JOIN_NOW_TEXT = "<code>Oh Dear,In Order To Use Me You Must Join My Update Channel</code>"
+    REPLY_TO_FILE = "Reply to that file with /rename new name.ext"
+    CONTACT_MY_DEVELOPER = "Something Wrong Contact My Developer 🤕"
+    REPLY_TO_VIDEO = "Reply to that file with /rename_video Newname.mkv"
+    START_TEXT = "Exclusive ! 📝 Rename Bot With Permanent Thumbnail Support!!!✔️\nBot By @TN57_BotZ 🤓"
+    UPGRADE_TEXT = "To upgrade your subscription <a href='https://t.me/BLuVDS'>[ Click Here ]</a>"
